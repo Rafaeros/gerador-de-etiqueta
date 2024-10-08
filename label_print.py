658383733e09f1a52a7ace2a7252d5f3d9467c21
Rafael Costa <rafaeladriancordeirocosta@gmail.com>
2024-10-08 20:21:39 -0300

    Refatorado o código para teste

--- label_print.py
+++ label_print.py
@@ -4,12 +4,13 @@ from barcode import Code128
 from barcode.writer import ImageWriter
 from win32 import win32print, win32api
 import os
+from io import BytesIO
 
 class LabelPrint():
   def __init__(self, LabelInfo) -> None:
     self.label_info = LabelInfo
 
-  def mm_to_px(self, mm, DPI=300) -> int:
+  def mm_to_px(self, mm: float, DPI: int =300) -> int:
     return int(mm * (DPI / 25.4))
 
   def wrap_text(self, text: str, max_chars: int = 35) -> str:
@@ -37,96 +38,70 @@ class LabelPrint():
 
     return wrapped_text
 
-  def description_barcode(self, text) -> None:
-    barcode: Code128 = Code128(text, writer=ImageWriter())
-    options: dict = {
-      "module_width": 0.35,                     
-      "module_height": 12, 
-      "quiet_zone": 1, 
-      "font_path": "FiraCode-Regular.ttf", 
-      "font_size": 10
-    }
-    barcode.save("description_barcode", options)
-
-  def quantity_barcode(self, text) -> None:
-    barcode: Code128 = Code128(text, writer=ImageWriter())
-    options: dict = {
-      "module_width": 0.35,                     
-      "module_height": 6, 
-      "quiet_zone": 1, 
-      "font_path": "FiraCode-Regular.ttf", 
-      "font_size": 10
-    }
-    barcode.save("quantity_barcode", options)
+  def draw_label_elements(self, image: Image, draw: ImageDraw, attributes: list[dict], font_path: str) -> None:
+    for attribute in attributes:
+      if 'width' in attribute:
+        barcode_buffer: BytesIO = BytesIO()
+        barcode: Code128 = Code128(attribute['text'], writer=ImageWriter())
+        options: dict = {
+          "module_width": attribute['width'],                
+          "module_height": attribute['height'], 
+          "quiet_zone": 1, 
+          "font_path": font_path, 
+          "font_size": 10
+        }
+        barcode.write(barcode_buffer, options)
+        barcode_image: Image = Image.open(barcode_buffer)
+        image.paste(barcode_image, attribute['xy'])
+      else:
+        draw.text(attribute['xy'], attribute['text'], font=attribute['font'], fill='black')
 
   def create_label(self) -> None:
     WIDTH: int = self.mm_to_px(150)
     HEIGHT: int = self.mm_to_px(100)
-    FONT_NAME: str = "FiraCode-Regular.ttf"
+    FONT_PATH: str = "./assets/fonts/FiraCode-Regular.ttf"
 
     # Posições dos elementos da etiqueta (label)
-    positions: dict = {
-      'date': (self.mm_to_px(10), self.mm_to_px(20)),
-      'boxes': (self.mm_to_px(50), self.mm_to_px(20)),
-      'line': (self.mm_to_px(0), self.mm_to_px(26), self.mm_to_px(160), self.mm_to_px(26)),
-      'client': (self.mm_to_px(5), self.mm_to_px(27)),
-      'code': (self.mm_to_px(5), self.mm_to_px(40)),
-      'description': (self.mm_to_px(5), self.mm_to_px(50)),
-      'description_barcode': (self.mm_to_px(5), self.mm_to_px(70)),
-      'quantity': (self.mm_to_px(5),self.mm_to_px(90)),
-      'quantity_barcode': (self.mm_to_px(70), self.mm_to_px(88)),
-      'weight': (self.mm_to_px(120), self.mm_to_px(90))
-    }
+    attr: list[dict] = [
+      {'xy': (self.mm_to_px(10), self.mm_to_px(20)), 'text': f'{self.label_info.date}', 'font': ImageFont.truetype(FONT_PATH, size=62)},
+      {'xy': (self.mm_to_px(50), self.mm_to_px(20)), 'text': f'{self.label_info.boxes}', 'font': ImageFont.truetype(FONT_PATH, size=62)},
+      {'xy': (self.mm_to_px(5), self.mm_to_px(27)), 'text': f'CLIENTE: {self.wrap_text(self.label_info.client)}', 'font': ImageFont.truetype(FONT_PATH, size=55)},
+      {'xy': (self.mm_to_px(5), self.mm_to_px(40)), 'text': f'CÓDIGO: {self.label_info.code}', 'font': ImageFont.truetype(FONT_PATH, size=80)},
+      {'xy': (self.mm_to_px(5), self.mm_to_px(50)), 'text': f'DESCRICÃO: {self.wrap_text(self.label_info.description)}', 'font': ImageFont.truetype(FONT_PATH, size=62)},
+      {'xy': (self.mm_to_px(5), self.mm_to_px(70)), 'text': f'{self.label_info.barcode}', 'font': ImageFont.truetype(FONT_PATH, size=62), 'width': 0.35, 'height': 12},  
+      {'xy': (self.mm_to_px(5),self.mm_to_px(90)), 'text': f'QUANTIDADE: {self.label_info.quantity}', 'font': ImageFont.truetype(FONT_PATH, size=62)},
+      {'xy': (self.mm_to_px(70), self.mm_to_px(88)), 'text': f'{self.label_info.quantity}', 'font': ImageFont.truetype(FONT_PATH, size=62), 'width': 0.35, 'height': 10},
+      {'xy': (self.mm_to_px(120), self.mm_to_px(90)), 'text': f'{self.label_info.weight} Kg', 'font': ImageFont.truetype(FONT_PATH, size=62)},
+    ]
 
     # Criar imagem em branco (label)
     label: Image = Image.new('RGB', (WIDTH, HEIGHT), color='white')
+
     # Criar objeto para desenhar na imagem
     draw: ImageDraw.Draw = ImageDraw.Draw(label)
 
-    # Tamanho das fontes
-    font: ImageFont = ImageFont.truetype(FONT_NAME, size=62)
-    code_font: ImageFont = ImageFont.truetype(FONT_NAME, size=80)
-    client_font: ImageFont = ImageFont.truetype(FONT_NAME, size=55)
-
-    # Passando o texto para quebrar a linha para se adequear a etiqueta
-    client: str = self.wrap_text(self.label_info.client, 45)
-    description: str = self.wrap_text(self.label_info.description)
-    self.description_barcode(self.label_info.barcode)
-    self.quantity_barcode(str(self.label_info.quantity))
-
-
-    # Desenhando elementos
-    draw.text(positions.get('date'), f'{self.label_info.date}', fill='black', font=font)
-    draw.text(positions.get('boxes'), str(self.label_info.boxes), fill='black', font=font)
-    draw.line(positions.get('line'), fill='black', width=3)
-    draw.text(positions.get('client'), f"CLIENTE: {client}",fill='black', font=client_font)
-    draw.text(positions.get('code'), f"CÓDIGO: {self.label_info.code}", fill='black', font=code_font)
-    draw.text(positions.get('description'), f"DESCRIÇÃO: {description}", fill='black', font=font)
-
-    # Codigo de barras da descrição
-    description_barcode = Image.open('description_barcode.png')
-    label.paste(description_barcode, positions.get('description_barcode'))
-
-    draw.text(positions.get('quantity'), f"QUANTIDADE: {str(self.label_info.quantity)} UND", fill='black', font=font)
-
-    # Codig de barras da quantidade
-    quantity_barcode = Image.open('quantity_barcode.png')
-    label.paste(quantity_barcode, positions.get('quantity_barcode'))
-
-    draw.text(positions.get('weight'), f"{self.label_info.weight} KG", fill='black', font=font)
+    # Desenhando elementos do label
+    draw.line((self.mm_to_px(0), self.mm_to_px(26), self.mm_to_px(160), self.mm_to_px(26)), fill='black', width=2)
+    self.draw_label_elements(label, draw, attr, FONT_PATH)
 
     label.thumbnail((WIDTH, HEIGHT))
-    label.save('etq.png')
+    label.save(f'./tmp/etq.png')
 
-  def print_label(self, file_path: str = './etq.png') -> None:
+  def print_label(self, file_path: str = './tmp/etq.png') -> None:
     abs_file_path: str = os.path.abspath(file_path)
     
     # Configuração impressora
     default_printer: str = win32print.GetDefaultPrinter()
     hprinter = win32print.OpenPrinter(default_printer)
-
-    try:  
-      win32api.ShellExecute(0,"print", abs_file_path, None, ".", 0)
+      
+    try:
+      win32print.StartDocPrinter(hprinter, 1, ("Print PNG", None, "RAW"))
+      win32print.StartPagePrinter(hprinter)
+      with open(abs_file_path, 'rb') as img:
+        win32print.WritePrinter(hprinter, img.read())
+      win32print.EndPagePrinter(hprinter)
+      win32print.EndDocPrinter(hprinter)
+      #win32api.ShellExecute(0, "print", abs_file_path, None, ".", 0)
     except Exception as e:
       print("Erro ao enviar para impressão: ", e)
     finally:
