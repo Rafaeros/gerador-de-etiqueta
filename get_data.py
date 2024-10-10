f4384d296fa823c729303771ef2697287e3f8c06
Rafael Costa <rafaeladriancordeirocosta@gmail.com>
2024-10-09 21:34:49 -0300

    Adicionado outro formato de data nos atributos, pegar apenas o texto do get_client_code (sem parenteses)

--- get_data.py
+++ get_data.py
@@ -5,28 +5,30 @@ import re
 file_date = dt.now().strftime("%d_%m_%Y")
 
 class LabelInfo:
-  def __init__(self, client: str, code: str, description: str, quantity: int, boxes: int, weight: int) -> None:
-    today: str = dt.now().strftime("%d/%m/%Y")
-    self.date: str = today
+  def __init__(self, op: str, client: str, code: str, description: str, quantity: int, boxes: int, weight: int) -> None:
+    today: str = dt.now()
+    self.date: str = today.strftime("%d/%m/%Y")
+    self.mwm_date: str = today.strftime("%Y%m%d")
+    self.op: str = op
     self.client: str = client
     self.code: str = code
     self.barcode: str = ""
+    self.client_code: str = ""
     self.description: str = description
     self.quantity: int = quantity
     self.weight: int = weight
     self.boxes: int = boxes
     self.set_barcode_data()
 
-  def get_client_code(self, string) -> str:
-    pattern = r'\((.*?)\)'
-    search = re.search(pattern, string)
+  def get_client_code(self, text: str) -> str:
+    search = re.search(r'\((.*?)\)', text)
     if search:
-      return search.group(0)
+      return search.group(1)
     return ''
 
   def set_barcode_data(self) -> str:
-    clientcode = self.get_client_code(self.description)
-    self.barcode = self.code + " " + clientcode
+    self.client_code = self.get_client_code(self.description)
+    self.barcode = self.code + " " + f"({self.client_code})"
     return self.barcode
 
 class LabelData:
@@ -51,7 +53,7 @@ class LabelData:
   def get_data(self, op: str, boxes: int, kg: str) -> LabelInfo | None:
     try:
       current_data = self.label_data.loc[self.label_data['Código']==op, ['Cliente', 'Cód. Material', 'Material', 'Quantidade']]
-      current_label = LabelInfo(current_data.loc[:,['Cliente']].to_string(index=False, header=False), current_data.loc[:,['Cód. Material']].to_string(index=False, header=False), current_data.loc[:,['Material']].to_string(index=False, header=False), current_data.loc[:,['Quantidade']].to_string(index=False, header=False), boxes, kg)
+      current_label = LabelInfo(op, current_data.loc[:,['Cliente']].to_string(index=False, header=False), current_data.loc[:,['Cód. Material']].to_string(index=False, header=False), current_data.loc[:,['Material']].to_string(index=False, header=False), current_data.loc[:,['Quantidade']].to_string(index=False, header=False), boxes, kg)
       return current_label
     except Exception as e:
       print(f"not found {e}")
